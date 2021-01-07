8d817cbd2ee32f6241b928b86802c83c5ab4fdec
adelh98 <adelh@uia.no>
2021-01-07 14:38:24 +0100

    First Commit, Workspace configured and ready to go

---
+++ FirstMod/src/main/java/com/thespartan/firstmod/FirstMod.java
@@ -0,0 +1,10 @@
+package com.thespartan.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FirstMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
